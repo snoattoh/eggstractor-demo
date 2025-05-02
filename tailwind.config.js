a913d7692a20846d9e0b8a089e1b062e67f02256
Stefan Attoh <snoattoh@gmail.com>
2025-05-02 02:24:38 -0600

    (PD-568) Convert expected output from Eggstractor into Moody's-ready code

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        white: "#ffffff",
+        black: "#000000",
+        blue: {
+          100: "#E0F5FF",
+          200: "#B2E5FF",
+          400: "#0093dc",
+          500: "#3dbeff",
+          900: "#003955",
+        },
+        green: {
+          100: "#d9f3bc",
+          300: "#76c022",
+        },
+      },
+    },
+  },
+};
